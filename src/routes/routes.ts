d3ec010837c2751c77c343c159612de49b5415a4
besaklish <besaklish@gmail.com>
2024-05-18 12:29:22 +0900

    feat: add route to ShowBlogPage

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -1,8 +1,10 @@
 import ListBlogPage from '@/pages/ListBlogPage.vue'
+import ShowBlogPage from '@/pages/ShowBlogPage.vue'
 import WriteBlogPage from '@/pages/WriteBlogPage.vue'
 import type { RouteRecordRaw } from 'vue-router'
 
 export const routes: RouteRecordRaw[] = [
   { path: '/', component: ListBlogPage, name: 'list-blog' },
-  { path: '/write', component: WriteBlogPage, name: 'write-blog' }
+  { path: '/write', component: WriteBlogPage, name: 'write-blog' },
+  { path: '/articles/:id', component: ShowBlogPage, name: 'show-blog' }
 ]
